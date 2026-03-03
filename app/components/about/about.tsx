843f12ea6148cb3a933fd3c86203b9b488d9394e
TheTexta <dextery777@gmail.com>
2026-03-02 19:38:08 -0500

    layout changes

--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -1,8 +1,25 @@
 export default function About() {
-    return (
-        <div className="mx-auto text-center">
-        <h1 className="text-5xl sm:text-7xl font-bold">Dexter Young</h1>
-        <p>CS Student at McGill</p>
-        </div>
-    );
+  return (
+    <div className="mx-auto text-center mt-32">
+      <h1 className="text-5xl sm:text-7xl font-bold">Dexter Young</h1>
+      
+
+      {/* <p>CS Student at McGill</p>
+      <h2 className="text-4xl">Contact me:</h2> */}
+      <div className="flex justify-center gap-5 mt-5 text-lg">
+        <a href="https://github.com/TheTexta" className="hover:underline">
+          GitHub: @TheTexta
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dexter-y"
+          className="hover:underline"
+        >
+          LinkedIn: dexter-y
+        </a>
+        <a href="mailto:dextery777@gmail.com" className="hover:underline">
+          Email: dextery777@gmail.com
+        </a>
+      </div>
+    </div>
+  );
 }
--- app/components/projects/projects-section.tsx
+++ app/components/projects/projects-section.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function ProjectsSection({ children }: { children: ReactNode }) {
   return (
     <>
-      <h2 className="text-4xl text-center mt-10 mb-5">Projects</h2>
+      <h2 className="text-4xl text-center mt-20 mb-5">Projects</h2>
       <div className="">
         {children}
       </div>
